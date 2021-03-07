323c554c6aaaaace3f764771f2222ee0983b0c64
Justin Marshall <justinmarshall20@gmail.com>
2021-03-07 11:27:18 -0800

    Engine side will now use Vector Macros instead of h2common; this fixes quite a few bugs(things don't fall through the floors).

--- recon/Programming/GameCode/game/q_shared.h
+++ recon/Programming/GameCode/game/q_shared.h
@@ -46,6 +46,18 @@
 #define NULL ((void *)0)
 #endif
 
+// jmarshall - this fixes some bugs on the engine side, don't know if the other game modules should have this or not.
+#ifdef QUAKE2
+#define DotProduct(x,y)			(x[0]*y[0]+x[1]*y[1]+x[2]*y[2])
+#define VectorSubtract(a,b,c)	(c[0]=a[0]-b[0],c[1]=a[1]-b[1],c[2]=a[2]-b[2])
+#define VectorAdd(a,b,c)		(c[0]=a[0]+b[0],c[1]=a[1]+b[1],c[2]=a[2]+b[2])
+#define VectorCopy(a,b)			(b[0]=a[0],b[1]=a[1],b[2]=a[2])
+#define VectorClear(a)			(a[0]=a[1]=a[2]=0)
+#define VectorNegate(a,b)		(b[0]=-a[0],b[1]=-a[1],b[2]=-a[2])
+#define VectorSet(v, x, y, z)	(v[0]=(x), v[1]=(y), v[2]=(z))
+void AngleVectors(vec3_t angles, vec3_t forward, vec3_t right, vec3_t up);
+#endif
+// jmarshall end
 
 // angle indexes
 #define	PITCH				0		// up / down
